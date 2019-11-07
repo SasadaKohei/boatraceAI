22271a11f56c6883d31cf61d0000b2182f3b613b
sasadakouhei <c501937014@tokushima-u.ac.jp>
2019-11-07 18:24:00 +0900

    add remove venue list

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,10 @@
-//会場リスト
+/**
+ *
+ *  クリックアクション、ページの作成
+ *
+ */
+
+//会場リスト(24個)
 const venue = ['桐生', '戸田', '江戸川', '平和島', '多摩川', '浜名湖', '蒲郡', '常滑', '津', '三国', '琵琶湖', '住之江', '尼崎',
     '鳴門', '丸亀', '児島', '宮島', '徳山', '下関', '若松', '芦屋', '福岡', '唐津', '大村'];
 
@@ -19,45 +25,45 @@ function createContentsMenu() {
 
 /**
  * createVenueSelectList()
- * role :選択可能会場を表示(開催中の会場を色付き, その他はグレーで表示)
+ * role :右コンテンツの選択可能会場を表示(開催中の会場を色付き, その他はグレーで表示)
  *
  * */
-// function createVenueSelectList() {
-//     let div = document.getElementById("venueSelectList");
-//
-//     for (let count = 0; count < venue.length; count++) {
-//         let cvs = document.createElement('button');
-//         let text = document.createTextNode(venue[count]);
-//         cvs.appendChild(text);
-//         cvs.setAttribute('id', 'venue_button_' + count);
-//         //後にスクレイピングでフラグを追加
-//         if (String(venue[count]) === '鳴門') {
-//             cvs.setAttribute('class', 'open_venue');
-//             cvs.addEventListener('click', onClickVenueButton);
-//         } else {
-//             cvs.setAttribute('class', 'rest_venue');
-//         }
-//         div.appendChild(cvs);
-//     }
-// }
+function createVenueSelectList() {
+    let div = document.getElementById("venueSelectList");
+
+    for (let count = 0; count < venue.length; count++) {
+        let cvs = document.createElement('button');
+        let text = document.createTextNode(venue[count]);
+        cvs.appendChild(text);
+        cvs.setAttribute('id', 'venue_button_' + count);
+        //後にスクレイピング競技開催中の会場フラグを追加
+        if (String(venue[count]) === '鳴門') {
+            cvs.setAttribute('class', 'open_venue');
+            cvs.addEventListener('click', onClickVenueButton);
+        } else {
+            cvs.setAttribute('class', 'rest_venue');
+        }
+        div.appendChild(cvs);
+    }
+}
 
 /**
  * onClickVenueButton()
  * role :開催中の会場をクリックした時の処理
  */
-// function onClickVenueButton() {
-//     changeRightVenueLayout(this.id);
-//     changeRightLayout();
-// }
+function onClickVenueButton() {
+    changeRightLayout();
+    changeRightVenueLayout(this.id);
+}
 
 /**
  * changeRightLayout()
  * role :開催中の会場をクリックした時の右レイアウトの背景変更処理
  */
-// function changeRightLayout() {
-//     let right_layout = document.getElementById('contentsRight');
-//     right_layout.className = 'contents_right_after';
-// }
+function changeRightLayout() {
+    let right_layout = document.getElementById('contentsRight');
+    right_layout.className = 'contents_right_after';
+}
 
 /**
  * changeRightLayout()
@@ -65,21 +71,62 @@ function createContentsMenu() {
  * @param clicked_id :押した会場ボタンのid
  */
 
-// function changeRightVenueLayout(clicked_id) {
-//     let clicked_venue = document.getElementById(clicked_id);
-//     clicked_venue.className = 'clicked_venue';
+function changeRightVenueLayout(clicked_id) {
+    let clicked_venue = document.getElementById(clicked_id);
+    clicked_venue.className = 'clicked_venue';
+
+    let remove_open_venue = document.getElementsByClassName('open_venue');
+    let remove_rest_venue = document.getElementsByClassName('rest_venue');
+    let remove_select_venue = document.getElementById("venueSelect");
+
+    let len_remove_open = remove_rest_venue.length;
+    let len_remove_rest = remove_open_venue.length;
+
+    // remove_rest_len配列の先頭を消していく
+    for (let i = 0; i < len_remove_rest; i++) {
+        remove_rest_venue[0].remove();
+    }
+
+    for (let i = 0; i < len_remove_open; i++) {
+        remove_rest_venue[0].remove();
+    }
+
+    remove_select_venue.remove();
+}
+
+// /**
+//  * checkOpenVenue()
+//  * role : 競技中の会場かチェック
+//  */
 //
-//     let remove_open_venue = document.getElementsByClassName('open_venue');
-//     let remove_rest_venue = document.getElementsByClassName('rest_venue');
-//     let len_remove_open = remove_open_venue.length;
-//     let len_remove_rest = remove_rest_venue.length;
-//
-//     for (let i = 0; i < len_remove_open; i++){
-//         remove_open_venue[i].remove();
-//     }
-//
-//     //謎エラーfor文抜けちゃう
-//     for (let i = 0; i < len_remove_rest; i++){
-//         remove_rest_venue[i].remove();
-//     }
+// function checkOpenVenue(){
+//     if (this.className === 'open_venue') onClickVenueButton();
 // }
+//
+// /**
+//  * addClickEvent()
+//  * role : クリックイベントを付け足す関数
+//  * @param count: 会場リストのindex番号
+//  */
+// function addClickEvent(count) {
+//     document.getElementById("venue_button_" + count).addEventListener('click', onClickVenueButton, false);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
